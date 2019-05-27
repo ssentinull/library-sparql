d756862c19ccfccae60af1ea27e5db15b16080ff
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-27 08:51:29 +0700

    feat: updt search query to get book cat, auth, pub, lib

--- repo/book_repo.js
+++ repo/book_repo.js
@@ -12,13 +12,24 @@ exports.getBooks = async name => {
   const queryData = {
     query: 
     `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX t: <http://www.semanticweb.org/boi/ontologies/2019/4/untitled-ontology-3#>
+    PREFIX t: <http://www.semanticweb.org/boi/ontologies/2019/4/perpustakaan#>
     
-    SELECT ?books
+    SELECT ?name ?category ?author ?publisher ?library
     WHERE {
       ?books rdf:type t:Book;
-              t:name ?name.
+        t:name ?name.
+
       filter regex(?name, "^${name}").
+      
+      ?books t:kindOf ?categoryInstance;
+        t:writtenBy ?authorInstance;
+        t:publishedBy ?publisherInstance;
+        t:archivedIn ?libraryInstance.
+      
+      ?categoryInstance t:name ?category.
+      ?authorInstance t:name ?author.
+      ?publisherInstance t:name ?publisher.
+      ?libraryInstance t:name ?library.
     }`
   };
 
