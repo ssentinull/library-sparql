a0c230e770acc47850aac446478e6c8edb0414e0
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-28 23:00:43 +0700

    feat: sparql get book suggestions

--- repo/book_repo.js
+++ repo/book_repo.js
@@ -40,7 +40,54 @@ exports.getBooks = async name => {
       data: qs.stringify(queryData)
     });
 
-    return Promise.resolve(data);
+    return data;
+  }
+  catch(err){
+    console.error(err);
+    return Promise.reject(err);
+  }
+}
+
+exports.getSuggestions = async categories => {
+  try{
+    const suggestions = [];
+
+    for(const category of categories){
+
+      const queryData = {
+        query: 
+        `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX t: <http://www.semanticweb.org/boi/ontologies/2019/4/perpustakaan#>
+        
+        SELECT ?name ?category ?author ?publisher ?library
+        WHERE {
+          ?books rdf:type t:Book;
+            t:name ?name;
+            t:kindOf ?categoryInstance;
+            t:writtenBy ?authorInstance;
+            t:publishedBy ?publisherInstance;
+            t:archivedIn ?libraryInstance.
+
+          ?categoryInstance t:name ?category.
+          ?authorInstance t:name ?author.
+          ?publisherInstance t:name ?publisher.
+          ?libraryInstance t:name ?library.
+    
+          filter regex(?category, "^${category}").
+        }
+        LIMIT 3`
+      };
+
+      const { data } = await axios(`${BASE_URL}/perpustakaan/query`, {
+        method: 'POST',
+        headers,
+        data: qs.stringify(queryData)
+      });
+
+      suggestions.push(data);
+    }
+
+    return suggestions;
   }
   catch(err){
     console.error(err);
